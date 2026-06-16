a1cba6fb62b14a8524a41bf6961db9d414cc7114
Mayank Agrawal <76840318+Mayank30-creator@users.noreply.github.com>
2026-06-17 02:14:39 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int farthest=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]+i>=farthest){
+                farthest=i;
+            }
+        }
+        return (farthest==0);
+    }
+}
